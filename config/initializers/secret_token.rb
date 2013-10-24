a5de3d62fc20762d783caa9294b0090f85a65b18
Jessie A. Young <jessie.a.young@gmail.com>
2013-10-23 20:29:19 -0700

    Suspended project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GratefulBecause::Application.config.secret_key_base = 'd39761fedf0bc3d0fd73f74681821666ec5f2971fd4ef74ed5133ff3567792bd6995765a099a77044e346a5c9cc4f8c20bc02cef51680025358bbb4d4c8c8a36'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GratefulBecause::Application.config.session_store :cookie_store, key: '_grateful-because_session'
